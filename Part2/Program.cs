058f83beee840ad9efc6aab4fabbd98aea30bc57
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-08-31 17:59:50 -0700

    Add Overall Plan to Part 2

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -1,4 +1,16 @@
-﻿
+﻿/// Chapter No. 1		Exercise No. 2
+/// File Name: Program.cs
+/// @author: Stephanie Zacharias
+/// Date:  August 30, 2020
+///
+/// Problem Statement: Use .WriteLine() to create a shape on the console screen.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Use WriteLine method to draw a shape on the screen
+/// 
+///
+
 using System;
 
 namespace Part2
